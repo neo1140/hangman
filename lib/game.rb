f41e728fdf870c34c56cbc1293c66ded672c667c
Zach Ickes <zachickes@hotmail.com>
2021-08-20 13:18:03 -0600

    got the save function to work, next work on loading

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,8 @@
 class Game
   attr_reader :guesses, :word, :masked_word, :game_over
 
-  def initialize(word='', guesses=0, masked_word='')
+  # Starts a new game, or loads a saved game
+  def initialize(word = '', guesses = 0, masked_word = '')
     @word = word
     @guesses = guesses
     @masked_word = masked_word
@@ -12,34 +13,43 @@ class Game
     p @masked_word
   end
 
+  # Fetches a random word from the dictionary file
   def random_word
     dictionary = File.readlines('5desk.txt')
-    wordchoice = rand(0..61405)
+    wordchoice = rand(0..61_405)
     dictionary.each_with_index { |word, index| return word.chomp.downcase if index == wordchoice }
   end
 
+  # Covers up a new word for display
   def word_mask(word)
     mask = ''
     mask.rjust(word.length, '-')
   end
 
+  # Updates display when user inputs the correct letter
+  def update(input)
+    @word.split('').each_with_index do |letter, index|
+      next unless letter == input
+
+      word_array = @masked_word.split('')
+      word_array[index] = letter
+      @masked_word = word_array.join
+    end
+    p @masked_word
+  end
+
+  # Checks for correct input, and advances the game
   def game_turn(input)
     p @masked_word
     if @word.include?(input)
-      @word.split('').each_with_index do |letter, index|
-        if letter == input
-          word_array = @masked_word.split('')
-          word_array[index] = letter
-          @masked_word = word_array.join
-        end
-      end
-      p @masked_word
+      update(input)
     else
       @guesses += 1
     end
     game_end if @guesses == 10 || @masked_word == @word
   end
 
+  # Sets game state to over
   def game_end
     @game_over = true
   end
@@ -47,11 +57,12 @@ end
 
 def save_game(game)
   Dir.mkdir('saves') unless Dir.exist?('saves')
-  filename = "saves/#{@word}.txt"
+  filename = "saves/hangman_#{game.masked_word}.txt"
   File.open(filename, 'w') do |file|
     file.puts "#{game.word}, #{game.guesses}, #{game.masked_word}"
   end
   puts 'game saved!'
+  Dir.children('saves').each_with_index { |save, index| puts "#{index}: #{save}" }
   game.game_end
 end
 
